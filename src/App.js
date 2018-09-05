a625f85b73d6eb41e77ff653b765c6b8e551a7a0
victoriachanimal <victoriakay.chan@gmail.com>
2018-09-05 17:39:20 -0400

    map

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import GoogleMapsContainer from './GoogleMapsContainer';
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-
           <h1 className="App-title">WE'RE GOING TO AFRICA</h1>
-
-
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+          </header>
+          < GoogleMapsContainer/>
       </div>
     );
   }
 }
 
-export default App;
+export default App; 
+
