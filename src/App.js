2f2abf60db3ca904f591c375a3cffdc204f59d4f
Pratik <pgauchan@gmail.com>
2018-09-05 17:28:44 -0400

    added search

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Search from './Components/Search';
+
+
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
 
-          <h1 className="App-title">WE'RE GOING TO AFRICA</h1>
+	
+	getSearchTerm = (e) => {
+		e.preventDefault();
+		console.log(e);
+	}
 
-
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+	render() {
+		return (
+		<div className="App">
+			<Search/>
+		</div>
+		);
+	}
 }
 
 export default App;
