d0303ccd292760ab989a7cdfb0a70ac83e7b55e0
victoriachanimal <victoriakay.chan@gmail.com>
2018-09-07 16:15:39 -0400

    button and more

--- src/Components/ConcertList/ConcertList.js
+++ src/Components/ConcertList/ConcertList.js
@@ -1,29 +1,32 @@
 import React, { Component } from 'react';
 import GoogleMapsContainer from '../map/GoogleMapsContainer';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
 class ConcertList extends Component {
     constructor(props) {
         super(props);
     }
     render() {
-
         return (
-            <div className="wrapper">
-                <h1>Concert List</h1>
+            <Router>
+                {/* <Route exact path="/ConcertList" component={ConcertList} /> */}
+                <div className="wrapper">
+                    <h1>Concert List</h1>
 
-                <div className="concertResults" concerts={this.props.concerts} >
-                    {this.props.concerts.map((concert) => {
+                    {/* <div className="concertResults" concerts={this.props.concerts} >
+                        {this.props.concerts.map((concert) => {
 
-                        return (
-                            <ul>
-                                <li>{concert.name}</li>
-                                <li>{concert.url}</li>
-                                <li>{concert.dates.start.localDate}, {concert.dates.start.localTime}</li>
-                            </ul>
-                        )
-                    })}
+                            return (
+                                <ul>
+                                    <li>{concert.name}</li>
+                                    <li>{concert.url}</li>
+                                    <li>{concert.dates.start.localDate}, {concert.dates.start.localTime}</li>
+                                </ul>
+                            )
+                        })}
+                    </div> */}
                 </div>
-            </div>
+            </Router >
         )
     }
 }
--- src/Components/results/Results.js
+++ src/Components/results/Results.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import GoogleMapsContainer from '../map/GoogleMapsContainer';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+
+import ConcertList from '../ConcertList/ConcertList';
 
 class Results extends Component {
   constructor(props) {
@@ -8,12 +11,18 @@ class Results extends Component {
   render() {
   
     return (
-      <div className="wrapper">
-        <h1>Results Page</h1>
-
-        <GoogleMapsContainer location={this.props.location}/>
-      </div>
-    )
+      <Router>
+        <div className="wrapper">
+          <h1>Results Page</h1>
+          <div className="resultsContainer">
+            <Link to="/ConcertList">Concert Listing</Link>
+            <Route exact path="/ConcertList" component={ConcertList} />
+          </div>
+          
+          <GoogleMapsContainer location={this.props.location} />
+        </div>
+      </Router >
+    );
   }
 }
 
