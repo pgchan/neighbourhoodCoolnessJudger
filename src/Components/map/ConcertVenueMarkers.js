b8d406fb6dbef18ba998016014db1f7582ce4fda
David <davidsavtran@gmail.com>
2018-09-09 13:15:17 -0400

    adding the ability to click a marker and show very basic info

--- src/Components/map/ConcertVenueMarkers.js
+++ src/Components/map/ConcertVenueMarkers.js
@@ -1,16 +1,44 @@
-import React from "react";
-import { Marker } from "react-google-maps";
+import React, {Component} from "react";
+import { Marker, InfoWindow } from "react-google-maps";
 import coolIcon from '../../styles/assets/cool.png';
 
+class ConcertVenueMarkers extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      markerClick: false,
+    }
+  }
+  clickMarker = () => {
+    this.setState({
+      markerClick: true,
+    })
+  }
+  closeMarker = () => {
+    this.setState({
+      markerClick: false,
+    })
+  }
+  render() {
+    return(
+      <Marker
+        position={this.props.location}
+        icon={coolIcon}
+        onClick={this.clickMarker}
+      >
 
-const ConcertVenueMarkers = (props) => {
-  return(
-    <Marker
-      position={props.location}
-      icon={coolIcon}
-    >
-    </Marker>
-  );
+        {this.state.markerClick ? 
+          <InfoWindow>
+            <div>
+              <p>{this.props.concertVenue._embedded.venues[0].name}</p>
+              <p>Address: {this.props.concertVenue._embedded.venues[0].address.line1}</p>
+            </div>
+          </InfoWindow>
+         : null
+        }
+      </Marker>
+    );
+  }
 }
 
 export default ConcertVenueMarkers;
--- src/Components/map/GoogleMaps.js
+++ src/Components/map/GoogleMaps.js
@@ -4,8 +4,9 @@ import LibraryMarkers from "./LibraryMarkers";
 import ConcertVenueMarkers from './ConcertVenueMarkers';
 
 const GoogleMaps = withScriptjs(withGoogleMap((props) => {
+
   const libraryLocations = props.libraries.map((place) => {
-    return <LibraryMarkers location={place.geometry.location}/>
+    return <LibraryMarkers location={place.geometry.location} library={place}/>
   });
   
   let concertVenueLocations;
@@ -13,11 +14,10 @@ const GoogleMaps = withScriptjs(withGoogleMap((props) => {
   if (Array.isArray(props.concerts)) {
     concertVenueLocations = props.concerts.map((venue) => {
       const location = venue._embedded.venues[0].location;
-      return <ConcertVenueMarkers location={{lat: parseFloat(location.latitude), lng: parseFloat(location.longitude)}}/>
+      return <ConcertVenueMarkers location={{lat: parseFloat(location.latitude), lng: parseFloat(location.longitude)}} concertVenue={venue}/>
     });
   }
   return (
-    console.log(props.concerts),
     <GoogleMap defaultZoom={12} center={props.location}>
       {libraryLocations}
       {concertVenueLocations}
